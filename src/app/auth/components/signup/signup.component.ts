639f95d6a25e8158fa7796d38df2e60c43d97ed4
Agustin Meikle <113302366+AgusMeikle@users.noreply.github.com>
2024-11-13 13:38:26 -0300

    signup

---
+++ src/app/auth/components/signup/signup.component.ts
@@ -0,0 +1,144 @@
+import { UserSessionService } from './../../services/user-session.service';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators, AbstractControl, FormGroup } from '@angular/forms';
+import { User } from '../../../users/interface/user.interface';
+import { UserService } from '../../../users/services/user.service';
+import { Router } from '@angular/router';
+import { AccountService } from '../../../accounts/services/account.service';
+import { Account } from '../../../accounts/interface/account.intarface';
+import { AddressService } from '../../../addresses/service/address.service';
+import { Address } from '../../../addresses/interface/address.interface';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css',
+})
+export class SignupComponent{
+  account!: Account;
+ 
+  password_validation = '';
+  showPassword = false;
+  
+
+  togglePasswordVisibility(input: HTMLInputElement) {
+    input.type = this.showPassword ? 'password' : 'text';
+    this.showPassword = !this.showPassword;
+  }
+
+  fb = inject(FormBuilder);
+  sesionService = inject(UserSessionService)
+  userService = inject(UserService);
+  accountService = inject(AccountService);
+  addressService = inject(AddressService);
+  route = inject(Router);
+  
+  passwordValidator(control: AbstractControl) {
+    const value = control.value;
+    const hasUpperCase = /[A-Z]/.test(value);
+    const hasLowerCase = /[a-z]/.test(value);
+    const hasNumber = /[0-9]/.test(value);
+    
+    const valid = hasUpperCase && hasLowerCase && hasNumber
+    
+    return valid ? null : { passwordStrength: true };
+  }
+
+  matchPasswords(control: FormGroup) {
+    const password = control.get('hashed_password')?.value;
+    const confirmPassword = control.get('confirm_password')?.value;
+  
+    return password === confirmPassword ? null : { passwordsMismatch: true };
+  }
+  
+  formulario = this.fb.group(
+    {
+      name_user: ['', [Validators.required, Validators.minLength(4)]],
+      last_name: ['', [Validators.required, Validators.minLength(2)]],
+      real_name: ['', [Validators.required, Validators.minLength(4)]],
+      dni: ['', [Validators.required, Validators.pattern('^[0-9]*$'), Validators.minLength(7), Validators.maxLength(8)]],
+      hashed_password: ['', [Validators.required, Validators.minLength(6), this.passwordValidator.bind(this), this.matchPasswords.bind(this)]],
+      confirm_password: ['', [Validators.required, Validators.minLength(6), this.passwordValidator.bind(this), this.matchPasswords.bind(this)]]
+    },{ validators: this.matchPasswords }
+  );
+
+  addUsuario() {
+    if (this.formulario.invalid) return;
+    
+    const { confirm_password, ...user } = this.formulario.getRawValue();
+    const user1 = {
+      isActive: 'yes',
+      user_type: 'user',
+      ...user
+    };
+    this.agregarCliente((user1) as User);
+  }
+
+  generateRandomCBU() {
+    let cbu = '' ;
+    for (let i = 0; i < 22; i++) {
+        cbu += Math.floor(Math.random() * 10).toString();
+    }
+    return cbu;
+  }
+  
+  generateRandomAlias() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let alias = '';
+    for (let i = 0; i < 14; i++) {
+        alias += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return alias;
+  }
+
+  createAccount(id: number){
+    let cuenta = {
+      cbu: this.generateRandomCBU(),
+      alias: this.generateRandomAlias(),
+      account_type: 'Savings',
+      user_id: id,
+      overdraft_limit: 0
+    }
+    this.accountService.createAccount(cuenta).subscribe({
+      next: (id) => {
+        console.log("Caja de ahorros creada, id = " + id);
+      },
+      error: (error: Error) => {
+          console.log(error.message);
+      }
+    })
+
+  }
+
+  createAddress(id: number) {
+    const address = {} as Address;
+    this.addressService.createAddress(id, address).subscribe({
+      next: (addressId) => {
+        console.log('Dirección creada, id = ' + addressId);
+      },
+      error: (error: Error) => {
+        console.error('Error al crear dirección:', error.message);
+      }
+    });
+  }
+
+  agregarCliente(user: User) {
+    console.log(user);
+    this.userService.postUser(user).subscribe({
+      next: (response) => {
+        console.log('Usuario creado:', response);
+        this.sesionService.setUserId(response);
+        this.createAccount(response);
+        this.createAddress(response);
+        this.route.navigate(['update-profile/', response])
+      },
+      error: (error: Error) => {
+        console.error('Error al crear usuario:', error);
+      },
+    });
+  }
+}
+
