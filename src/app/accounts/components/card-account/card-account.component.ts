827f3f3f68bb8731493e80a7550e02b8e3e01beb
Agustin Meikle <113302366+AgusMeikle@users.noreply.github.com>
2024-11-13 13:31:12 -0300

    card-account

---
+++ src/app/accounts/components/card-account/card-account.component.ts
@@ -0,0 +1,26 @@
+import { UserSessionService } from './../../../auth/services/user-session.service';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Account } from '../../interface/account.intarface';
+
+@Component({
+  selector: 'app-card-account',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './card-account.component.html',
+  styleUrl: './card-account.component.css',
+})
+export class CardAccountComponent {
+  @Input()
+  account!: Account;
+
+  @Output()
+  navigateEvent = new EventEmitter<string>();
+
+  userSessionService = inject(UserSessionService);
+  userId: number = this.userSessionService.getUserId();
+
+  onNavigate(){
+    this.navigateEvent.emit(String(this.account.id));
+  }
+}
