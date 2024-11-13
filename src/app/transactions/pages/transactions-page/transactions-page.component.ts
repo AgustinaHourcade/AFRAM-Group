cc64c0782ef032650744d89151388701ecbaa9de
Agustin Meikle <113302366+AgusMeikle@users.noreply.github.com>
2024-11-13 13:51:28 -0300

    transactions-page

---
+++ src/app/transactions/pages/transactions-page/transactions-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { NavbarComponent } from '../../../shared/navbar/navbar.component';
+import { Router, RouterLink } from '@angular/router';
+import { TransferModalComponent } from "../../components/transfer-modal/transfer-modal.component";
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-transactions-page',
+  standalone: true,
+  imports: [NavbarComponent, RouterLink, TransferModalComponent, CommonModule],
+  templateUrl: './transactions-page.component.html',
+  styleUrl: './transactions-page.component.css'
+})
+export class TransactionsPageComponent{
+  isModalOpen = false;
+
+  router = inject(Router)
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+  goToNewTransfer(account: any) {
+    this.isModalOpen = false;
+    this.router.navigate(['/transactions/new-transfer'], { state: { account } });
+  }
+
+ 
+}
