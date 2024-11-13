87c2c2fdeb2b56fcb009d69670d1c0aced040734
franpedrosa10 <151399778+franpedrosa10@users.noreply.github.com>
2024-11-13 13:46:27 -0300

    chat message chatbot

---
+++ src/app/shared/chat-bot/components/chat-message/chat-message.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Message } from '../../interface/chat.interface';
+
+@Component({
+  selector: 'app-chat-message',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chat-message.component.html',
+  styleUrls: ['./chat-message.component.css']
+})
+export class ChatMessageComponent {
+  @Input() message!: Message;
+  @Output() faqSelected = new EventEmitter<string>();
+  @Output() topicSelected = new EventEmitter<string>();
+
+  selectFaq(question: string): void {
+    this.faqSelected.emit(question);
+  }
+
+  selectTopic(title: string): void {
+    this.topicSelected.emit(title);
+  }
+}
