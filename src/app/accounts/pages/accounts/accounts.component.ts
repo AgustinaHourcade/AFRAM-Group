107d72a57ae134a710f9e5fe5b6efdb5bd9c8788
AgustinaHourcade <136352454+AgustinaHourcade@users.noreply.github.com>
2024-11-13 13:32:56 -0300

    Accounts page

---
+++ src/app/accounts/pages/accounts/accounts.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import { NavbarComponent } from '../../../shared/navbar/navbar.component';
+import { AccountService } from '../../services/account.service';
+import { UserSessionService } from '../../../auth/services/user-session.service';
+import { CardAccountComponent } from '../../components/card-account/card-account.component';
+import { CbuAliasComponent } from '../../components/cbu-alias/cbu-alias.component';
+import { Account } from '../../interface/account.intarface';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-accounts',
+  standalone: true,
+  imports: [NavbarComponent, CardAccountComponent, CbuAliasComponent],
+  templateUrl: './accounts.component.html',
+  styleUrl: './accounts.component.css'
+})
+export class AccountsComponent {
+  accounts: Array<Account> = [];
+  userId: number = 0;
+
+  router = inject(Router);
+  userSessionService = inject(UserSessionService);
+  accountService = inject(AccountService);
+
+  ngOnInit(): void {
+    this.userId = this.userSessionService.getUserId();
+
+    this.accountService.getAccountsByIdentifier(Number(this.userId)).subscribe({
+      next: (accounts) => {
+        this.accounts = accounts;
+      },
+      error: (error: Error) => {
+        console.error('Error fetching accounts:', error);
+      }
+    });
+
+  }
+  
+  changeAccount(){
+    
+
+  }
+
+}
