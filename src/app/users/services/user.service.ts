0072267a1f5fec7fd8d1f0d016bc18c6311323f7
AgustinaHourcade <136352454+AgustinaHourcade@users.noreply.github.com>
2024-11-13 13:54:11 -0300

    User service

---
+++ src/app/users/services/user.service.ts
@@ -0,0 +1,39 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user.interface';
+import { Address } from '../../addresses/interface/address.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  http = inject(HttpClient);
+
+  private baseUrl = 'http://localhost:3000/users/';
+
+  getIdByDni(dni: string | undefined): Observable<number> {
+    return this.http.get<number>(`${this.baseUrl}id/${dni}`);
+  }
+
+  getUser(id: number): Observable<User> {
+    return this.http.get<User>(`${this.baseUrl}${id}`);
+  }
+
+  postUser(usuario: User): Observable<number> {
+    return this.http.post<number>(`${this.baseUrl}register`, usuario);
+  }
+
+  updateUser(datos: {phone: string, email: string}, id: number): Observable<User> {
+    return this.http.put<User>(`${this.baseUrl}update/${id}`, datos);
+  }
+
+  verifyUser(user: { username: string; dni: string; password: string }): Observable<Number> {
+    return this.http.post<Number>(`${this.baseUrl}verify`, user); 
+  }
+
+  changePassword(id: number,  datos: {currentPassword: string, newPassword: string}): Observable<any> {
+    return this.http.put<User>(`${this.baseUrl}change-password/${id}`, datos);
+  }
+
+}
