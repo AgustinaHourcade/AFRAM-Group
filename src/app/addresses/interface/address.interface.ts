135d1bced47f29863ae5631c97a3a5d4837ac1d8
AgustinaHourcade <136352454+AgustinaHourcade@users.noreply.github.com>
2024-11-13 13:34:54 -0300

    Address interface

---
+++ src/app/addresses/interface/address.interface.ts
@@ -0,0 +1,11 @@
+export interface Address {
+  street: string;
+  address_number: number;
+  floor?: string;
+  apartment?: string;
+  city: string;
+  postal_code: string;
+  country: string;
+  user_id: number;
+
+}
