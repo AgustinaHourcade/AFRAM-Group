bc924616dde1a894425ad2e1b8cd3862c0e50654
tute-vaccaro <136352472+tute-vaccaro@users.noreply.github.com>
2024-11-13 13:40:14 -0300

    card interface

---
+++ src/app/cards/interface/card.ts
@@ -0,0 +1,13 @@
+export interface Card {
+    cardId:number,
+    card_number: string,
+    cardHolder_name: string,
+    cardHolder_last_name: string,
+    issue_date: string,
+    expiration_date: string,
+    cvv: number,
+    card_type: string,
+    is_Active: string,
+    user_id: number,
+    account_id: number
+}
