95022fa1cb3f909cd3335a700956e2d48539c6f0
franpedrosa10 <151399778+franpedrosa10@users.noreply.github.com>
2024-11-13 13:51:51 -0300

    transaction service

---
+++ src/app/transactions/services/transaction.service.ts
@@ -0,0 +1,22 @@
+import { Transaction } from './../interface/transaction.interface';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService {
+  http = inject(HttpClient);
+  
+  private baseUrl = 'http://localhost:3000/transactions'; 
+
+  getTransactionsByAccountId(id: number): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(`${this.baseUrl}/account/${id}`);
+  }
+
+  postTransaction(transaction: Transaction): Observable<number>{
+    return this.http.post<number>(this.baseUrl, transaction);
+  }
+
+}
