975eab41823159fc7c5196be9c2c53d73e42408e
tute-vaccaro <136352472+tute-vaccaro@users.noreply.github.com>
2024-11-13 13:53:35 -0300

    user interface

---
+++ src/app/users/interface/user.interface.ts
@@ -0,0 +1,13 @@
+export interface User{
+    id?:number,
+    name_user: string,
+    last_name?: string,
+    hashed_password: string,
+    email?: string,
+    dni: string,
+    phone?: string,
+    user_type?: string,
+    isActive?: string,
+    real_name?: string
+}
+
