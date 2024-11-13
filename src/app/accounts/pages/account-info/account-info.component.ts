f9a75ac670d37fd9c267154da5a2fb1c85fc7816
tute-vaccaro <136352472+tute-vaccaro@users.noreply.github.com>
2024-11-13 13:32:52 -0300

    account-info component

---
+++ src/app/accounts/pages/account-info/account-info.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CbuAliasComponent } from '../../components/cbu-alias/cbu-alias.component';
+import { NavbarComponent } from '../../../shared/navbar/navbar.component';
+
+@Component({
+  selector: 'app-account-info',
+  standalone: true,
+  imports: [CbuAliasComponent, NavbarComponent],
+  templateUrl: './account-info.component.html',
+  styleUrl: './account-info.component.css'
+})
+export class AccountInfoComponent {
+
+}
