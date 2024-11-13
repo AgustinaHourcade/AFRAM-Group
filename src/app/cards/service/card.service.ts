9be2d176a8460531dfb72cd76811769f1e4630d3
franpedrosa10 <151399778+franpedrosa10@users.noreply.github.com>
2024-11-13 13:40:24 -0300

    card service

---
+++ src/app/cards/service/card.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Card } from '../interface/card';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+
+  constructor() { }
+
+  private http = inject(HttpClient);
+  private urlBase = 'http://localhost:3000/cards/';
+
+  getCards(): Observable<Card[]>{
+    return this.http.get<Card[]>(this.urlBase);
+  }
+
+  getCardsById(user_id: number): Observable<Card[]>{
+    return this.http.get<Card[]>(`${this.urlBase}user/${user_id}`);
+  }
+
+  createCard(card : Card): Observable<Card>{
+    return this.http.post<Card>(this.urlBase, card);
+  }
+
+  disableCard(cardId: number): Observable<boolean>{
+    return this.http.put<boolean>(`${this.urlBase}deactivate`, cardId);
+  }
+
+  
+}
