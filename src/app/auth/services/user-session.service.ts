7b87edae072260cc432eab7ded4f17d7e4ee3052
Agustina Hourcade <agushourcade@gmail.com>
2024-11-20 20:39:10 -0300

    Solución refrescar

--- src/app/auth/services/user-session.service.ts
+++ src/app/auth/services/user-session.service.ts
@@ -4,22 +4,25 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserSessionService {
-  private userIdKey: number = 0;
-  
+  private readonly USER_ID_KEY = 'userId';
   estoyLogeado: boolean = false;
 
+  // Guarda el ID en sessionStorage
   setUserId(id: number): void {
-    this.userIdKey = id;
+    sessionStorage.setItem(this.USER_ID_KEY, id.toString());
   }
 
-  clearUserId(): void {
-    this.userIdKey = 0;
-  }
-
+  // Obtiene el ID del usuario como número
   getUserId(): number {
-    return this.userIdKey;
+    const storedValue = sessionStorage.getItem(this.USER_ID_KEY);
+    return storedValue ? Number(storedValue) : -1;
   }
 
+  // Limpia el ID del usuario
+  clearUserId(): void {
+    sessionStorage.removeItem(this.USER_ID_KEY);
+  }
+  
   getLogIn(){
     return this.estoyLogeado
   }  
@@ -31,6 +34,4 @@ export class UserSessionService {
   logOut(){
     this.estoyLogeado = true;
   }
-
-  
 }
