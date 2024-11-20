e3de2f33269080ca36803530bf080582c0d4c82c
Agustina Hourcade <agushourcade@gmail.com>
2024-11-20 18:49:10 -0300

    Navbar Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,12 +2,11 @@ import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HomeCardComponent } from '../../shared/home-card/home-card.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
-import { NavbarHomeComponent } from "../../shared/navbar-home/navbar-home.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeCardComponent, FooterComponent, NavbarHomeComponent],
+  imports: [HomeCardComponent, FooterComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
